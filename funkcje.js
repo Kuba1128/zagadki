af19f9d8c6d9da0d6bc509a4bf5d858a1c8ab26b
Kuba1128 <66248294+Kuba1128@users.noreply.github.com>
2022-02-11 09:51:42 +0100

    Add files via upload

---
+++ funkcje.js
@@ -0,0 +1,40 @@
+function f1()
+{
+    document.getElementById("d1").innerHTML="Odpowiedź do zagadki pierwszej: Jedna bogini dostała jabłko z koszykiem";
+}
+function f2()
+{
+    document.getElementById("d1").innerHTML="Odpowiedź do zagadki pierwszej: Ukryto.";
+} 
+function f3()
+{
+     document.getElementById("d2").innerHTML="Odpowiedź do zagadki drugiej: Litera: A";
+}
+function f4()
+{
+    document.getElementById("d2").innerHTML="Odpowiedź do zagadki drugiej: Ukryto.";
+}   
+function f5()
+{
+    document.getElementById("d3").innerHTML="Odpowiedź do zagadki trzeciej: Jutro.";
+}  
+function f6()
+{
+    document.getElementById("d3").innerHTML="Odpowiedź do zagadki trzeciej: Ukryto.";
+}
+function f7()
+{
+    document.getElementById("d4").innerHTML="Odpowiedź do zagadki czwartej: Mały, czerwony trójkąt";
+}
+function f8()
+{
+    document.getElementById("d4").innerHTML="Odpowiedź do zagadki czwartej: Ukryto.";
+}
+function f9()
+{
+    document.getElementById("d5").innerHTML="Odpowiedź do zagadki piątej: Jedno. Potem żołądek już nie będzie pusty.";
+}
+function f10()
+{
+    document.getElementById("d5").innerHTML="Odpowiedź do zagadki piątej: Ukryto.";
+}
